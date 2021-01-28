0e94561c1b641006e16dbcd3f205661e13ec905e
HALE KARA <haleburcinkarakaya@gmail.com>
2021-01-28 21:49:14 +0300

    Fixes

---
+++ MuratOdev/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MuratOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girilenSayi;
+            Console.WriteLine("Bir Tek Sayı giriniz: ");
+            girilenSayi = int.Parse(Console.ReadLine());
+            if (girilenSayi % 2 == 0)
+            {
+                Console.WriteLine("Lütfen tek sayı giriniz: ");
+            }
+            else
+            {
+                for (int k = 1; k <= girilenSayi+1; k=k+2)
+                {
+                    for (int m = 0; m < k; m=m+2)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int i = girilenSayi+1; i > k; i--)
+                    {
+                        
+                        Console.Write("*");
+                    }
+
+                    
+                    Console.Write("\n");
+                }
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
